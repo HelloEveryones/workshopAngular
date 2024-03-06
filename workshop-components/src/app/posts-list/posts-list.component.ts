e6e529f5337f50bfd6642b3fe20930a708c97cd8
Iva <iva_dkostadinova@abv.bg>
2024-03-06 18:48:20 +0200

    creat  type interface post

--- workshop-components/src/app/posts-list/posts-list.component.ts
+++ workshop-components/src/app/posts-list/posts-list.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiserviceService } from '../apiservice.service';
 
 @Component({
   selector: 'app-posts-list',
   templateUrl: './posts-list.component.html',
   styleUrls: ['./posts-list.component.css']
 })
-export class PostsListComponent {
-
+export class PostsListComponent implements OnInit{
+constructor(private api:ApiserviceService){}
+ngOnInit(): void {
+  this.api.getPosts().subscribe(posts=>{
+    console.log(posts);
+    
+  })
+}
 }
